b61fdda5eae3e37734f66a90eab257ab8bbfae0a
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-03-29 21:33:49 -0700

    Refactor

--- src/extratools_gittools/prompt.py
+++ src/extratools_gittools/prompt.py
@@ -5,6 +5,55 @@ from colors import color
 from .status import get_status
 
 
+def __get_branch_prompt(
+    branch: str | None,
+    *,
+    has_remote: bool,
+    ahead: int,
+    behind: int,
+) -> str:
+    if branch is None:
+        return color("(detached)", fg="red")
+
+    prompt: str = color(branch, fg=(
+        "green" if has_remote else "cyan"
+    ))
+
+    if has_remote:
+        remote_flags: str = ""
+
+        if ahead > 0:
+            remote_flags += color('↑', fg="blue", style="bold")
+        if behind > 0:
+            remote_flags += color('↓', fg="yellow", style="bold")
+
+        if remote_flags:
+            prompt += remote_flags
+
+    return prompt
+
+
+def __get_local_flags(
+    *,
+    staged: bool,
+    unstaged: bool,
+    untracked: bool,
+) -> str:
+    local_flags: str = ""
+
+    if staged:
+        local_flags += color('+', fg="green", style="bold")
+    if unstaged:
+        local_flags += color('*', fg="red", style="bold")
+    if untracked:
+        local_flags += color('?', fg="cyan", style="bold")
+
+    if local_flags:
+        return ':' + local_flags
+
+    return ""
+
+
 def get_prompt() -> str:
     status: dict[str, Any] | None = get_status()
     if status is None:
@@ -18,39 +67,16 @@ def get_prompt() -> str:
     unstaged = bool(status["files"]["unstaged"])
     untracked = bool(status["files"]["untracked"])
 
-    prompt: str = ""
-
-    if branch is None:
-        prompt += color("(detached)", fg="red")
-    else:
-        prompt += color(branch, fg=(
-            "green" if has_remote else "cyan"
-        ))
-
-        if has_remote:
-            remote_flags: str = ""
-
-            if ahead > 0:
-                remote_flags += color('↑', fg="blue", style="bold")
-            if behind > 0:
-                remote_flags += color('↓', fg="yellow", style="bold")
-
-            if remote_flags:
-                prompt += remote_flags
-
-    local_flags: str = ""
-
-    if staged:
-        local_flags += color('+', fg="green", style="bold")
-    if unstaged:
-        local_flags += color('*', fg="red", style="bold")
-    if untracked:
-        local_flags += color('?', fg="cyan", style="bold")
-
-    if local_flags:
-        prompt += ':' + local_flags
-
-    return prompt
+    return __get_branch_prompt(
+        branch,
+        has_remote=has_remote,
+        ahead=ahead,
+        behind=behind,
+    ) + __get_local_flags(
+        staged=staged,
+        unstaged=unstaged,
+        untracked=untracked,
+    )
 
 
 def print_prompt() -> None:
--- src/extratools_gittools/status.py
+++ src/extratools_gittools/status.py
@@ -33,35 +33,39 @@ def parse_status(output: str) -> dict[str, Any]:
     untracked: list[str] = []
 
     for line in output.rstrip('\n').splitlines():
-        if line.startswith('#'):
-            if line.startswith("# branch.oid "):
-                oid = line.rsplit(' ', 1)[1]
-            if line.startswith("# branch.head "):
-                branch = line.rsplit(' ', 1)[1]
-                if branch != "(detached)":
-                    head = branch
-            elif line.startswith("# branch.upstream "):
-                branch = line.rsplit(' ', 1)[1]
-                if branch != "(detached)":
-                    upstream = branch
-            elif line.startswith("# branch.ab "):
-                ahead, behind = [abs(int(x)) for x in line.rsplit(' ', 2)[1:]]
-        elif line.startswith('?'):
-            untracked.append(line.rsplit(' ', -1)[1])
-        elif not line.startswith('!'):
-            vals: list[str] = line.split(' ')
+        vals: list[str] = line.split(' ')
 
-            path: str = vals[-1]
+        match vals[0]:
+            case '#':
+                match vals[1]:
+                    case "branch.oid":
+                        oid = vals[2]
+                    case "branch.head":
+                        branch = vals[2]
+                        if branch != "(detached)":
+                            head = branch
+                    case "branch.upstream":
+                        branch = vals[2]
+                        if branch != "(detached)":
+                            upstream = branch
+                    case "branch.ab":
+                        ahead, behind = [abs(int(x)) for x in vals[2:]]
+            case '?':
+                untracked.append(vals[1])
+            case '!':
+                pass
+            case _:
+                path: str = vals[-1]
 
-            submodule_flags: str = vals[2]
-            if submodule_flags[0] == 'S' and submodule_flags[3] == 'U':
-                untracked.append(path)
+                stage_flags: str = vals[1]
+                if stage_flags[0] != '.':
+                    staged.append(path)
+                if stage_flags[1] != '.':
+                    unstaged.append(path)
 
-            stage_flags: str = vals[1]
-            if stage_flags[0] != '.':
-                staged.append(path)
-            if stage_flags[1] != '.':
-                unstaged.append(path)
+                submodule_flags: str = vals[2]
+                if submodule_flags[0] == 'S' and submodule_flags[3] == 'U':
+                    untracked.append(path)
 
     return {
         "oid": oid,
